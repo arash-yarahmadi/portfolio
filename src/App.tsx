8b4ac308c6fae306fcf8edf57b54fa96c8cb280d
arash-yarahmadi <arashyarahmadi90@yahoo.com>
2025-10-29 12:48:18 +0330

    implement portfolio page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AuthenticationPage from "./pages/Authentication";
-import LandingPage from "./pages/Landing";
+//import AuthenticationPage from "./pages/Authentication";
+//import LandingPage from "./pages/Landing";
+import Portfolio from "./pages/Portfolio";
 
 export default function App() {
   return (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/authenticate" element={<AuthenticationPage />} />
+        <Route path="/" element={<Portfolio />} /> 
       </Routes>
     </BrowserRouter>
   );
