b017b45a9239c7858671621abd7b9673e6bd2fdb
arash-yarahmadi <arashyarahmadi90@yahoo.com>
2025-10-29 14:28:07 +0330

    feat:landing page implement

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 //import AuthenticationPage from "./pages/Authentication";
-//import LandingPage from "./pages/Landing";
-import Portfolio from "./pages/Portfolio";
+import LandingPage from "./pages/Landing";
+//import Portfolio from "./pages/Portfolio";
 
 export default function App() {
   return (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
-        <Route path="/" element={<Portfolio />} /> 
+        <Route path="/" element={<LandingPage />} /> 
       </Routes>
     </BrowserRouter>
   );
